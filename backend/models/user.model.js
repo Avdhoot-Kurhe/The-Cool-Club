3dfcf7d88e7cee484afe384dd2a3c46b8f85d4cd
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-11-09 09:37:58 +0530

    update:user,review,cart models

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema=mongoose.Schema({
+const userSchema= new mongoose.Schema({
     firstname:{type:String, required:true},
     lastname:{type:String, required:true},
     email:{type:String, required:true},
