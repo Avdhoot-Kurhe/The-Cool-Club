a7809fa1d864ed2f2ff53d38d6d3ba1c9b131b57
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-11-09 09:21:19 +0530

    add:usermodel

---
+++ backend/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema=mongoose.Schema({
+    firstname:{type:String, required:true},
+    lastname:{type:String, required:true},
+    email:{type:String, required:true},
+    password:{type:String, required:true},
+    postalcode:{type:Number, required:true},
+    phone:{type:Number, required:true},
+    role:{type:String, required:true}
+});
+
+const UserModel=mongoose.model('user',userSchema);
+
+module.exports={
+    UserModel,
+}
