ccb5ef240acd19a6f2f945fc7c4abd38b246d5b4
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-11-09 19:54:27 +0530

    update:payment and review routes

---
+++ backend/controllers/review.controller.js
@@ -0,0 +1,14 @@
+
+
+const getReviews=async(req,res)=>{try{}catch(err){}}
+const addReviews=async(req,res)=>{try{}catch(err){}}
+const updateReviews=async(req,res)=>{try{}catch(err){}}
+const deleteReviews=async(req,res)=>{try{}catch(err){}}
+
+const ReviewController={
+    getReviews,addReviews,updateReviews,deleteReviews
+}
+
+module.exports={
+    ReviewController
+}
