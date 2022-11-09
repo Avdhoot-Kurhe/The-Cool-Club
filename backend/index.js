bd5d8fd39b076573b67e47f4ffad15ee1b776fd7
Avdhoot-Kurhe <101327844+Avdhoot-Kurhe@users.noreply.github.com>
2022-11-09 12:13:32 +0530

    index.js structure added

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+require("dotenv").config();
+const port=process.env.PORT
+const { connection } = require('./config/db.config');
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send("Welcome to the Backend!");
+})
+
+app.listen(port,async()=>{
+    try{
+        await connection
+        console.log("Connected To MongoDB");
+    }catch(e){
+        console.log(404,"Couldn't connect");
+    }
+    console.log("connected to server")
+})
