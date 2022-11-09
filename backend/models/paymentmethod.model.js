81807eb5862c93adc7e672bd634ecbc4733d0196
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-11-09 10:12:56 +0530

    add:middleware files

---
+++ backend/models/paymentmethod.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const paymentmethodSchema= new mongoose.Schema({
+    nameoncard:{type:String, required:true},
+    uid:{type:String, required:true},
+    cardnumber:{type:Number, required:true},
+    cardtype:{type:String, required:true},
+    expiry:{type:Date, required:true},
+},{timestamps:true});
+
+const PaymentmethodModel=mongoose.model('paymentmethod',paymentmethodSchema);
+
+module.exports={
+    PaymentmethodModel,
+}
