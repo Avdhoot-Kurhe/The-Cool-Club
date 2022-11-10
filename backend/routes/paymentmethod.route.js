d692d72970897a0391565e719eef0894b9b8ead9
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-11-10 11:46:00 +0530

    update:paymentmethod controller

--- backend/routes/paymentmethod.route.js
+++ backend/routes/paymentmethod.route.js
@@ -1,8 +1,12 @@
 const {Router} = require('express');
+const { PaymentMethodController } = require('../controllers/paymentmethod.controller');
 
 const paymentMethodRouter=Router();
 
-
+paymentMethodRouter.get('/',PaymentMethodController.getPaymentMethod)
+paymentMethodRouter.post('/create',PaymentMethodController.addPaymentMethod)
+paymentMethodRouter.put('/:id',PaymentMethodController.updatePaymentMethod)
+paymentMethodRouter.delete('/:id',PaymentMethodController.deletePaymentMethod)
 
 
 
