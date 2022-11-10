65dfb2095f8d0a0c5173c711a93675149abf5a18
Akshay Puranik <akshay.social@gmail.com>
2022-11-10 19:28:36 +0530

    Added: Banners

--- Frontend/thecoolclub/src/Akshay/Pages/Components/GoodThings.jsx
+++ Frontend/thecoolclub/src/Akshay/Pages/Components/GoodThings.jsx
@@ -1,4 +1,13 @@
-import { Center, Grid, GridItem, Image, Text, VStack } from "@chakra-ui/react";
+import {
+    ButtonGroup,
+  Center,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 
 const tabsData = [
@@ -34,30 +43,56 @@ const tabsData = [
   },
 ];
 
+const buttonNames = [
+    {
+        
+    }
+]
+
 const GoodThings = () => {
   return (
-    <Center w="100%">
-      <Grid
-        templateColumns={{
-          md: "repeat(3,1fr)",
-          base: "repeat(2,1fr)",
-          lg: "repeat(6,1fr)",
-        }}
-        p="2%"
-        gap="5"
-      >
-        {tabsData.map((el) => {
-          return (
-            <GridItem>
+    <>
+      <Center w="100%">
+        <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw9a12e321/images/Christmas2022/xcat_moxy-launch_fa3_hb.jpg?yocs=s_" />
+      </Center>
+      <ButtonGroup></ButtonGroup>
+      <Center w="100%">
+        <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw126cc0be/images/Christmas2022/xcat_hp-gifting-zone_c1_hb.jpg?yocs=s_" />
+      </Center>
+      <Heading fontSize="24px" textAlign="center" p="2%">
+        MORE GOOD THINGS, THIS WAY
+      </Heading>
+      <Center w="100%">
+        <Grid
+          w="80%"
+          templateColumns={{
+            md: "repeat(3,1fr)",
+            base: "repeat(2,1fr)",
+            lg: "repeat(6,1fr)",
+          }}
+          p="2%"
+          gap="5"
+        >
+          {tabsData.map((el) => {
+            return (
+              <GridItem>
                 <VStack>
-                    <Image src={el.image} />
-                    <Text>{el.title}</Text>
+                  <Image src={el.image} />
+                  <Text
+                    textAlign="center"
+                    fontSize="14px"
+                    textDecoration="underline"
+                    fontWeight="semibold"
+                  >
+                    {el.title}
+                  </Text>
                 </VStack>
-            </GridItem>
-          );
-        })}
-      </Grid>
-    </Center>
+              </GridItem>
+            );
+          })}
+        </Grid>
+      </Center>
+    </>
   );
 };
 
