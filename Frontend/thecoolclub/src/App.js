ea6ec9056b833eb9e020efa31cd5cecb09069b0e
pankajsajwan12 <75352393+pankajsajwan12@users.noreply.github.com>
2022-11-12 03:22:57 +0530

    add signup and login page

--- Frontend/thecoolclub/src/App.js
+++ Frontend/thecoolclub/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Login from './components/pankajsingh/Login/Login';
 import BillingPage from './components/pankajsingh/Shipping/BillingPage';
+import SignUp from './components/pankajsingh/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
       {/* <Bodycarepage /> */}
      {/* <ShippingPage /> */}
-     <BillingPage />
+     {/* <BillingPage /> */}
+     <Login />
+     {/* <SignUp /> */}
     </div>
   );
 }
