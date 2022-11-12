06795c023736763ed6278310cc039c2a3dc2822b
pankajsajwan12 <75352393+pankajsajwan12@users.noreply.github.com>
2022-11-12 10:42:19 +0530

    delete app

--- Frontend/thecoolclub/src/Redux/store.js
+++ Frontend/thecoolclub/src/Redux/store.js
@@ -9,7 +9,7 @@ export const store = legacy_createStore(AppReducer, applyMiddleware(thunk));
 
 import { applyMiddleware, combineReducers, legacy_createStore} from 'redux';
 import thunk from 'redux-thunk'
-// import { reducer as App } from './App/reducer';
+import { reducer as AppReducer } from './AppReducer/reducer';
 import { reducer as Auth } from './Auth/reducer';
 
 const rootReducer = combineReducers({Auth })
